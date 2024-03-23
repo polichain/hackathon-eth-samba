bc31514d2d1da67fbcd2f35e3dde080f0840dbaf
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-03-23 18:07:17 -0300

    novo_contrato

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -2,13 +2,22 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Rent {
-    uint public timeRemaining; // Duration of the rental agreement in months
-    uint public rentAmount; // Monthly rent amount in wei
-    uint public insuranceAmount; // Insurance amount in wei
-    //uint public fine; //amount of the insurance that must be taken when the contract is aborted or something goes wrong
-    address public admin; // Address of the admin
-    address payable public renter; // Address of the renter
-    address payable public locator; // Address of the entity receiving the rent
+
+    
+    bool private locked;
+    uint public next_index = 0;
+
+    struct Contract {
+        address payable renter;
+        address payable locator;
+        uint insuranceAmount;
+        uint rentAmount;
+        uint definedTime;
+        uint timeRemaining;
+        ContractState state;
+    }
+
+    mapping(uint => Contract) public rentContracts;
 
     enum ContractState { StandBy,Active, Aborted, Completed }
     ContractState public state;
@@ -18,81 +27,81 @@ contract Rent {
     event ContractAborted(address indexed sender);
     event ContractCompleted();
 
-    constructor(
-        uint _timeRemaining,
-        uint _rentAmount,
-        uint _insuranceAmount,
-       // uint _fine,
-        address _admin,
-        address payable _renter,
-        address payable _locator
-    ) {
-        timeRemaining = _timeRemaining;
-        rentAmount = _rentAmount;
-        insuranceAmount = _insuranceAmount;
-        renter = _renter;
-        locator = _locator;
-        admin = _admin;
-        //fine = _fine;
-    }
+    //modifier onlyRenter() {
+        //require(msg.sender == renter, "Only the renter can perform this action");
+        //_;
+    //}
 
-    modifier onlyRenter() {
-        require(msg.sender == renter, "Only the renter can perform this action");
+   // modifier onlyActiveContract() {
+       // require(state == ContractState.Active, "Contract is not active");
+        //_;
+   // }
+
+    modifier noReentrancy() {
+        require(!locked, "No reentrancy");
+        locked = true;
         _;
+        locked = false;
     }
 
-    modifier onlyActiveContract() {
-        require(state == ContractState.Active, "Contract is not active");
-        _;
+    function createNewContract(address payable _renter,
+     uint _insuranceAmount, uint _rentAmount, uint _definedTime) external {
+    // Ideally, include access control here (e.g., only the admin or locator can create a new contract)
+
+    Contract storage newContract = rentContracts[next_index];
+    newContract.renter = _renter;
+    newContract.locator = payable(msg.sender); // Assuming msg.sender is the locator
+    newContract.insuranceAmount = _insuranceAmount;
+    newContract.rentAmount = _rentAmount;
+    newContract.definedTime = _definedTime;
+    newContract.timeRemaining = _definedTime;
+    newContract.state = ContractState.StandBy;
+
+    next_index++;
     }
 
-    function payRent() external payable onlyRenter onlyActiveContract {
-       // require(msg.value >= (rentAmount), "Insufficient payment amount");
-        //uint excessAmount = msg.value - (rentAmount + insuranceAmount)*10**18;
-        //if (excessAmount > 0) {
-            //payable(msg.sender).transfer(excessAmount); // Refund excess amount
-       // }
+    function payRent(uint index) public payable noReentrancy{
 
-        locator.transfer(msg.value);
+       require(rentContracts[index].state == ContractState.Active,"The contract is not active");
+       rentContracts[index].locator.transfer(msg.value);
 
-        timeRemaining--;
+        rentContracts[index].timeRemaining--;
 
-        if (timeRemaining == 0) {
-            state = ContractState.Completed;
-            receiveInsurance();
+        if (rentContracts[index].timeRemaining == 0) {
+            rentContracts[index].state = ContractState.Completed;
+            receiveInsurance(index);
             emit ContractCompleted();
         }
         emit RentPaid(msg.sender, msg.value);
     }
 
-    function abortContract() public payable onlyActiveContract {
-        require(msg.sender == renter || msg.sender == locator, "Only the renter or locator can abort the contract");
+    function abortContract(uint index) public payable noReentrancy {
+        require(rentContracts[index].state == ContractState.Active,"The contract is not active");
+        require(rentContracts[index].renter == msg.sender || rentContracts[index].locator == msg.sender , "Only the renter or locator can abort the contract");
 
+           // uint time_percentage = definedTime*(70)/(100);
+            //if(msg.sender == renter && time_percentage < (definedTime - timeRemaining )){
+
+          //  }
         
-            (bool success, ) = renter.call{value: insuranceAmount}("");
+            (bool success, ) = rentContracts[index].renter.call{value: rentContracts[index].rentAmount }("");
             require(success, "transfer failed");
         
-        state = ContractState.Aborted;
+        rentContracts[index].state = ContractState.Aborted;
         emit ContractAborted(msg.sender);
     }
 
-    function payInsurance() public payable {
-        require(state == ContractState.StandBy, "Contract is in stand by");
-        require(msg.sender == renter && msg.value >= insuranceAmount, "Only the renter can do this operation and the insurance must be higher");
+    function payInsurance(uint index) public payable noReentrancy{
+        require(rentContracts[index].state == ContractState.StandBy, "Contract is not in stand by");
+        require(msg.sender == rentContracts[index].renter && msg.value >= rentContracts[index].insuranceAmount, "Only the renter can do this operation and the insurance must be higher");
 
-        //uint  excessAmount = msg.value -(insuranceAmount)*10**18;
-
-       // if(excessAmount > 0){
-           // payable(msg.sender).transfer(excessAmount);
-       // }
-
-        state = ContractState.Active;
+        rentContracts[index].state = ContractState.Active;
         emit InsurancePaid(msg.sender, msg.value, address(this));
     }
 
-    function receiveInsurance() internal {
+    function receiveInsurance(uint index) internal {
 
-         (bool success, ) = renter.call{value: insuranceAmount}("");
+         (bool success, ) = rentContracts[index].renter.call{value: rentContracts[index].insuranceAmount}("");
           require(success, "transfer failed");
 
     }
@@ -100,6 +109,5 @@ contract Rent {
   function contractBalance() public view returns (uint) {
     return address(this).balance;
 }
-
 
 }
